9ab52ab42f296d1afbceaea32f58a5919b98d131
Nick Trunov <trunov1997@yandex.ru>
2019-11-19 15:48:00 +0300

    Built Colors & Type page

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-            main: './src/js/index.js'},
+        common: './src/scss/common.scss',    
+        colorsType: './src/pages/colors-type/colors-type.js',
+    },
     output: {
             path: path.resolve(__dirname, 'build')
     },
@@ -30,13 +34,35 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: 'index.html',
+			filename: 'index.html',
 			template: './src/index.pug',
+			chunks: ['common']
+		}),
+
+		new HtmlWebpackPlugin({
+			filename: 'pages/colors-type.html',
+			template: './src/pages/colors-type/colors-type.pug',
+			chunks: ['common', 'colorsType']
         }),
-        new MiniCssExtractPlugin('style.css'),
+        
+        new MiniCssExtractPlugin({
+			filename: 'css/[name].css',
+			chunkFilename: '[id].css'
+        }),
+        
+        new CopyWebpackPlugin([
+			{
+				from: './src/img',
+				to: './img'
+			},
+			{
+				from: './src/fonts',
+				to: './fonts'
+			}
+		]),
     ],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
 		overlay: true,
-    }
+    },
 };
