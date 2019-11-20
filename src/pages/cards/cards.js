eacaf72b551d0725f5b9bf0dfd4db72c15d5fd84
Nick Trunov <trunov1997@yandex.ru>
2019-11-20 12:34:14 +0300

    Added register-card block

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -1,7 +1,7 @@
 import './cards.scss';
 import '../../blocks/find-apartments-card/find-apartments-card';
 import Calendar from '../../blocks/calendar/static-calendar';
-// import '../../blocks/register-card/register-card';
+import '../../blocks/register-card/register-card';
 // import '../../blocks/enter-card/enter-card';
 // import '../../blocks/image-card/image-card';
 // import '../../blocks/booking-card/booking-card';
