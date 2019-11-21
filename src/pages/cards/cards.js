aaf0195564c9b2e5932701686503096d5e5f5242
Nick Trunov <trunov1997@yandex.ru>
2019-11-21 13:40:18 +0300

    Added enter-card block

--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -3,7 +3,7 @@ import '../../blocks/find-apartments-card/find-apartments-card';
 import Calendar from '../../blocks/calendar/static-calendar';
 import '../../blocks/register-card/register-card';
 import '../../blocks/booking-card/booking-card';
-// import '../../blocks/enter-card/enter-card';
+import '../../blocks/enter-card/enter-card';
 // import '../../blocks/image-card/image-card';
 
 Calendar('static_calendar')
