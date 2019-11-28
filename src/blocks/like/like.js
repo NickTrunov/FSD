29b0c43160cafee35d98b387b77d945afb4ed4cc
Nick Trunov <trunov1997@yandex.ru>
2019-11-28 17:55:21 +0300

    Added working like button

--- src/blocks/like/like.js
+++ src/blocks/like/like.js
@@ -1,1 +1,10 @@
 import './like.scss';
+
+$('.like').on('click', function(count) {
+    var $this = $(this),
+        count = $this.attr('data-count'),
+        active = $this.hasClass('active');
+  
+  $this.attr('data-count', active ? --count : ++count).toggleClass('active');
+
+  });
