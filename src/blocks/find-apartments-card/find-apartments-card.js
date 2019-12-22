27c13c8d2fa2bff403ece3bf8067906b191e9ca5
Nick Trunov <trunov1997@yandex.ru>
2019-12-22 13:25:05 +0300

    Added dropdown and responsive to Landing

--- src/blocks/find-apartments-card/find-apartments-card.js
+++ src/blocks/find-apartments-card/find-apartments-card.js
@@ -9,3 +9,27 @@ Calendar({
     container: $('#calendar'),
     inputs: $('#first_date,#second_date')
 })
+
+
+$('#dd').dropdown({
+    title: 'Гости',
+    placeholder: 'Сколько гостей',
+    singular: true,
+    buttons: true,
+    show: false,
+    mapNames: ['гость', 'гостя', 'гостей'],
+    elements: [
+        {
+            title: 'взрослые',
+            value: 2
+        },
+        {
+            title: 'дети',
+            value: 1
+        },
+        {
+            title: 'младенцы',
+            value: 0
+        }
+    ]
+});
