a47d2b19e72c788786d97d47ad070a3d1cabd2b7
Nick Trunov <trunov1997@yandex.ru>
2019-12-22 14:24:36 +0300

    Added Background block

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -2,3 +2,4 @@ import './landing.scss';
 import '../../blocks/header/header';
 import '../../blocks/footer/footer';
 import '../../blocks/find-apartments-card/find-apartments-card';
+import '../../blocks/background/background';
