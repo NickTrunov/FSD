2c1e03159431998b2fff20190b7838392c709001
Nick Trunov <trunov1997@yandex.ru>
2020-01-16 16:36:05 +0300

    Added Donut diagram

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'jquery-ui/ui/widgets/datepicker';
 import 'jquery.maskedinput/src/jquery.maskedinput';
 import './blocks/calendar/calendar';
 import './blocks/configuration-dropdown/dropdown-plugin/dropdown-plugin';
+import { Donut } from './blocks/donut-diagram/donut-diagram';
 
 function importAll(resolve) {
     resolve.keys().forEach(resolve);
@@ -10,3 +11,8 @@ function importAll(resolve) {
 importAll(require.context('../src/', true, /\.js$|\.scss$/));
 
 $('.js-masked').mask('99.99.9999');
+
+const pie = document.getElementsByClassName('js-donut'); 
+for (let item of pie) {
+    new Donut(item).draw();
+}
